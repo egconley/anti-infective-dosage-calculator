51fea09e67e8e4984176ec7c45b87d4c92822348
egconley <31393715+egconley@users.noreply.github.com>
2019-12-12 12:49:11 -0800

    m and f to male and female - tinkering with data

--- server.js
+++ server.js
@@ -19,11 +19,39 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 // Database queries
 // http://zetcode.com/javascript/nodepostgres/
 
-client.query('SELECT * FROM anti_microbial_drugs').then(res => {
+// express and exports - modularization and passing variables between js files
+// from https://stackoverflow.com/questions/9765215/global-variable-in-app-js-accessible-in-routes
+// // In app.js:
+// app.locals.variable_you_need = 42;
 
-  const fields = res.fields.map(field => field.name);
+// // In index.js
+// exports.route = function(req, res){
+//     var variable_i_needed = req.app.locals.variable_you_need;
+// }
+// app.locals documentation
+// http://expressjs.com/en/api.html#app.locals
+// artile to read: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 
-  console.log(fields);
+let allDrugNames = [];
+function Drug(drug) {
+  this.drug_name = drug;
+  allDrugNames.push(this);
+}
+
+client.query('SELECT drug_name FROM anti_microbial_drugs ORDER BY drug_name').then(res => {
+
+  // const fields = res.fields.map(field => field.name);
+  const drug_names = res.rows.map(name => name.drug_name);
+  // console.log('here: ', drug_names);
+  drug_names.forEach(drug_name => {
+    // console.log(drug_name);
+    new Drug(drug_name);
+    // console.log(drugInstance.drug_name);
+    // console.log(allDrugNames);
+    // $('select').append(`<option value=${drug}>${drug}</option>`);
+  })
+
+  // console.log(drug_names);
 
 }).catch(err => {
   console.log(err.stack);
