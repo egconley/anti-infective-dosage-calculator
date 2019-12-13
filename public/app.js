2316f356ca4e4222f51d18ff9be8d67373ff6f01
egconley <31393715+egconley@users.noreply.github.com>
2019-12-13 14:57:32 -0800

    console.logging selected drug from dropdown menu

--- public/app.js
+++ public/app.js
@@ -9,6 +9,12 @@ let creatinineVar;
 let userForm = document.getElementById('user-form');
 let appendHere = document.getElementById('appendTest');
 
+// Drop down menu
+$('select').on('change', function(){
+  let selectedDrug = $(this).val()
+  console.log(selectedDrug);
+})
+
 function Patient(sex, age, height, weight, creatinine) {
   this.sex = sex;
   this.age = age;
