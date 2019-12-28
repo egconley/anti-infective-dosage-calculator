e8ca591d2f54f72059f64069133e0a4d01f0e4a7
egconley <31393715+egconley@users.noreply.github.com>
2019-12-27 18:38:41 -0600

    updated schema file

--- data/uw_dosing_guidelines_schema.sql
+++ data/uw_dosing_guidelines_schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS anti_microbial_drugs (
   route VARCHAR(5),
   notes VARCHAR(255),
   agent_type VARCHAR(255),
-  multi_temp_var_name boolean
+  drug_name_short VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS dosing_by_CrCl_level (
@@ -12,5 +12,8 @@ CREATE TABLE IF NOT EXISTS dosing_by_CrCl_level (
   drug_name VARCHAR(255),
   CrCl_level VARCHAR(255),
   indication VARCHAR(255),
-  dose VARCHAR(255)
+  dose VARCHAR(255),
+  CrCl_cutoff_high float,
+  CrCl_cutoff_low float,
+  drug_name_short VARCHAR(255)
 );
