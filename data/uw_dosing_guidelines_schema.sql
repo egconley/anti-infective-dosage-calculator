d5c4eb203cd263d747a28c8534166127ca14f904
egconley <31393715+egconley@users.noreply.github.com>
2019-12-28 14:50:30 -0600

    updated data in pgAdmin and transferred to heroku, updated schema file

--- data/uw_dosing_guidelines_schema.sql
+++ data/uw_dosing_guidelines_schema.sql
@@ -1,6 +1,6 @@
 --heroku pg:psql -f uw_dosing_guidelines_schema.sql -a dosage-calculator
 
-DROP TABLE IF EXISTS anti_microbial_drugs;
+--DROP TABLE IF EXISTS anti_microbial_drugs;
 
 CREATE TABLE IF NOT EXISTS anti_microbial_drugs (
   id SERIAL PRIMARY KEY,
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS anti_microbial_drugs (
   drug_name_short VARCHAR(255)
 );
 
-\copy anti_microbial_drugs (drug_name, route, notes, agent_type, drug_name_short) FROM 'anti_microbial_drugsEXP-12-27-19.csv' WITH CSV HEADER; 
+\copy anti_microbial_drugs (drug_name, route, notes, agent_type, drug_name_short) FROM 'anti_microbial_drugsEXP-12-28-19.csv' WITH CSV HEADER; 
 
-DROP TABLE IF EXISTS dosing_by_CrCl_level;
+--DROP TABLE IF EXISTS dosing_by_CrCl_level;
 
 CREATE TABLE IF NOT EXISTS dosing_by_CrCl_level (
   id SERIAL PRIMARY KEY,
@@ -26,4 +26,21 @@ CREATE TABLE IF NOT EXISTS dosing_by_CrCl_level (
   drug_name_short VARCHAR(255)
 );
 
-\copy dosing_by_crcl_level (drug_name, crcl_level, indication, dose, crcl_cutoff_high, crcl_cutoff_low, drug_name_short) FROM 'dosing_by_crcl_levelEXP-12-27-19.csv' WITH CSV HEADER; 
+\copy dosing_by_crcl_level (drug_name, crcl_level, indication, dose, crcl_cutoff_high, crcl_cutoff_low, drug_name_short) FROM 'dosing_by_crcl_levelEXP-12-28-19.csv' WITH CSV HEADER; 
+
+--hd_dosingEXP-12-28-19.csv
+
+--DROP TABLE IF EXISTS hd_dosing;
+
+CREATE TABLE IF NOT EXISTS hd_dosing (
+  id SERIAL PRIMARY KEY,
+  drug_name VARCHAR(255),
+  CrCl_level VARCHAR(255),
+  indication VARCHAR(255),
+  dose VARCHAR(255),
+  CrCl_cutoff_high float,
+  CrCl_cutoff_low float,
+  drug_name_short VARCHAR(255)
+);
+
+\copy hd_dosing (drug_name, crcl_level, indication, dose, crcl_cutoff_high, crcl_cutoff_low, drug_name_short) FROM 'hd_dosingEXP-12-28-19.csv' WITH CSV HEADER; 
