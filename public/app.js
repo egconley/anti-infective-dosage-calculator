38b45e37fa1204ee1b4ff3bb492387de15bd3e94
egconley <31393715+egconley@users.noreply.github.com>
2019-12-30 15:07:18 -0600

    change button text with HD toggle

--- public/app.js
+++ public/app.js
@@ -72,8 +72,10 @@ function displayPatientInputForm() {
   let hdValue = $('input[name=hd]:checked').val();
   if (hdValue === 'no') {
     $everythingAfterHDRadio.css("display", "block");
+    $('button').text('Calculate CrCl / Get Dose Guidance');
   } else if (hdValue === 'yes') {
     $everythingAfterHDRadio.css("display", "none");
     $('button').css("display", "block");
+    $('button').text('Get Dose Guidance');
   }
 }
