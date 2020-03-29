9e2747fb3793b0a08c95c82edd017f99afbbe7b7
egconley <31393715+egconley@users.noreply.github.com>
2020-03-29 15:46:18 -0700

    commented in error handlers

--- server.js
+++ server.js
@@ -190,24 +190,24 @@ app.post('/dose', urlencodedParser, function (req, res) {
 
 // development error handler
 // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function (err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // // production error handler
 // // no stacktraces leaked to user
-// app.use(function (err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
